52dc2d42ea2d2a291572a397488c067c5b2f03ab
Weihao Rick Lee <kenchi133@hotmail.com>
2016-09-14 21:33:47 -0500

    initialize firebase connection

---
+++ javascript/moment.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBUIF90q59JOuKU7JcId2pAVAVUnXLKeQw",
+    authDomain: "traintimetable-6667f.firebaseapp.com",
+    databaseURL: "https://traintimetable-6667f.firebaseio.com",
+    storageBucket: "",
+    messagingSenderId: "57971458789"
+  };
+  firebase.initializeApp(config);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
